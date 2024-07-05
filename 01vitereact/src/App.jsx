5ad211a655b7eedaf9e5fd322fb420b7845b4951
jaydipsudani <jaydipsudani@gmail.com>
2024-07-05 23:49:41 +0530

    first react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,11 @@
-
+import Chai from "./chai" 
 
 function App() {
   return (
+    <>
+    <Chai/>
           <h1>Jaydip sudani with vite</h1>
+          </>
   )
 }
 export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai() {
+    return(
+        <h1>hello React is ready</h1>
+    )
+}
+export default Chai
