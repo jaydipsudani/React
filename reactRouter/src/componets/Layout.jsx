cb3a52f91637620d38afef65babb1020891bdc6b
jaydipsudani <jaydipsudani@gmail.com>
2024-07-19 17:33:57 +0530

    update

--- reactRouter/src/componets/Layout.jsx
+++ reactRouter/src/componets/Layout.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './componets/Header/Header'
-import Footer from './componets/Footer/Footer'
+import 
 function Layout() {
     return (
         <>
-    <header/>
-    <Outlet/>
-    <footer/>
+
         </>
     )
 }
