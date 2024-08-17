28c5e1566a16312c2ef956e74c2dcb7f5ac0d392
jaydipsudani <jaydipsudani@gmail.com>
2024-08-17 16:40:38 +0530

    Update Github.jsx

--- practice1/src/componets/Github.jsx
+++ practice1/src/componets/Github.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
   //   const [data,setData]  = useState([])
@@ -11,22 +11,20 @@ function Github() {
   //   } catch (error) {
   //     setData(error)
   //   }
-    
-    
+
   //  },[])
-  const data = useLoaderData(); 
+  const data = useLoaderData();
   return (
-    <div className='text-center bg-gray-600 text-white p-4 text-3xl'>
-        Github Followers: {data.followers}
-        <img  className=' w-40' src={data.avatar_url} />
-        
-         </div>
-  )
+    <div className="text-center bg-gray-600 text-white p-4 text-3xl">
+      Github Followers: {data.followers}
+      <img className=" w-40" src={data.avatar_url} />
+    </div>
+  );
 }
 
-export default Github
+export default Github;
 
-export const githubInfoLoader = async()=>{
-  const responce = await fetch('https://api.github.com/users/hiteshchoudhary')
-  return responce.json()
-}
+export const githubInfoLoader = async () => {
+  const responce = await fetch("https://api.github.com/users/hiteshchoudhary");
+  return responce.json();
+};
