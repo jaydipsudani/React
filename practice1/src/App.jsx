f07432749e80c333271cc2a47964b7957c1878c2
jaydipsudani <jaydipsudani@gmail.com>
2024-08-17 09:01:16 +0530

    route

--- practice1/src/App.jsx
+++ practice1/src/App.jsx
@@ -1,26 +1,42 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
+import {BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Outlet,
+} from 'react-router-dom'
 
-export default function App() {
-  const {
-    register,
-    handleSubmit,
-    formState:{errors}
+function App() {
 
-  } = useForm();
-
-  const onsubmit = (data) => console.log(data);
-  
   return (
-   <form onSubmit={handleSubmit(onsubmit)}>
-    <input {...register("firstname",{required:true})}/>
-    {errors.firstname && <span className='text-red-500'>This field is reqired</span>}
-    <select {...register("gender")}>
-    <option value="female">Female</option>
-    <option value="male">Male</option>
-    <option value="other">Other</option>
-    </select>
-    <input type='submit'/>
-   </form>
+    <>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {/* <li>
+            <Link to="/about">About</Link>
+          </li> */}
+          <li>
+            <Link to="/company"> Company</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/"  element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route Path="/Company" element={<Company/>} />
+      </Routes>
+    </Router>
+    </>
   )
 }
+
+const Home = ()=> <h1>Home Page</h1>
+const About = ()=> { return <h1 className='text-green-400'>About Us</h1>}
+const Company = ()=> { return <h1 className='text-pink-400'>Company</h1>}
+
+export default App
